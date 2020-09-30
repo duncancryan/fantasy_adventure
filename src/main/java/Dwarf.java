fbc20eeb4493de66b23b4a896cafb789ac4062a2
Duncan Ryan <duncancryan1@gmail.com>
2020-09-30 14:32:42 +0100

    overriden methds in warrior classes

--- src/main/java/Dwarf.java
+++ src/main/java/Dwarf.java
@@ -3,4 +3,10 @@ public class Dwarf extends Warrior {
     public Dwarf(int health, String name, Weapon weapon) {
         super(health, name, weapon);
     }
+
+    public void attack(Character target){
+        int baseDamage = this.getWeapon().getDamage();
+        int damage = baseDamage * 2;
+        target.reduceHealth(damage);
+    }
 }
