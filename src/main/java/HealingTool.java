f62797cdf4d359cae9f731b56eb4c20cdbf75178
Duncan Ryan <duncancryan1@gmail.com>
2020-09-30 14:38:32 +0100

    created healingtool enum

--- src/main/java/HealingTool.java
+++ src/main/java/HealingTool.java
@@ -1,2 +1,13 @@
 public enum HealingTool {
+    POTION("potion", 50),
+    LOTION("lotion", 20),
+    HERBS("herbs", 10);
+
+    private final String name;
+    private final int value;
+
+    HealingTool(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
 }
