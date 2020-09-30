57ea905d8d483ee7dafb24cfd2a976399d76d22e
Timothy Saudners <timsaunders88@gmail.com>
2020-09-30 15:32:52 +0100

    added game pseudocode

---
+++ src/main/java/Game/Game.java
@@ -0,0 +1,25 @@
+package Game;
+
+public class Game {
+
+    //set up sequence of rooms
+    //set up party of adventurers
+    //for each room
+        //if room is treasure
+            //pick it up
+        //if room is monstery
+            //fight monster
+
+
+    //fight monster
+        // do while roomComplete == false && adventurersAlive = true
+        //monster attacks random adventurer
+        //for each adventurer in party
+            // if of type IAttack
+                //attack monster
+            // if of type healer
+                // heal adventurer with lowest health
+
+
+
+}
